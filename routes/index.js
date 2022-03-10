960f6142d21047daa488e04a1d0ef26ad4acd96e
Edvaldo Szymonek <edvaldoszy@gmail.com>
2022-03-09 21:59:21 -0300

    Implementa rota de exclusão de tarefas

--- routes/index.js
+++ routes/index.js
@@ -81,13 +81,12 @@ router.delete('/usuarios/:usuarioId', async function (req, res) {
     await usuario.destroy();
     res.json({
       mensagem: 'Usuário excluído com sucesso',
-    })
+    });
   } else {
     res.json({
       mensagem: 'O usuário não existe',
     });
   }
-  
 });
 
 router.get('/tarefas', async function (req, res) {
@@ -107,7 +106,7 @@ router.post('/tarefas', async function (req, res) {
   res.json({
     mensagem: "Tarefa cadastrada com sucesso",
     tarefa: tarefa,
-  })
+  });
 });
 
 router.put('/tarefas/:tarefaId', async function (req, res) {
@@ -123,6 +122,23 @@ router.put('/tarefas/:tarefaId', async function (req, res) {
     mensagem: 'Tarefa alterada com sucesso',
     tarefa: tarefa,
   });
+});
+
+router.delete('/tarefas/:tarefaId', async function (req, res) {
+  const tarefa = await Tarefa
+    .where('id', req.params.tarefaId)
+    .fetch();
+  if (tarefa) {
+    // DELETE FROM tarefas WHERE id = req.params.tarefaId
+    tarefa.destroy();
+    res.json({
+      mensagem: 'Tarefa excluída com sucesso'
+    });
+  } else {
+    res.json({
+      mensagem: 'A tarefa não existe'
+    });
+  }
 });
 
 router.get('/usuarios/:usuarioId/tarefas', async function (req, res) {
